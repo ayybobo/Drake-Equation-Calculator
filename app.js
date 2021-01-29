4f098dcee9003c21418fcf8aa8ac639972edc122
ayybobo <71397971+ayybobo@users.noreply.github.com>
2021-01-28 20:42:15 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+function calc() {
+
+var a = parseFloat(+document.getElementById('a').value);
+var b = parseFloat(+document.getElementById('b').value);
+var c = parseFloat(+document.getElementById('c').value);
+var d = parseFloat(+document.getElementById('d').value);
+var e = parseFloat(+document.getElementById('e').value);
+var f = parseFloat(+document.getElementById('f').value);
+var g = parseFloat(+document.getElementById('g').value);
+
+var result = parseInt(document.getElementById("result").value = a*b*c*d*e*f*g);
+
+return result;
+
+};
+
+
